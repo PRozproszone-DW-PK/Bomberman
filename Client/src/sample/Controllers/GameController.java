4b2b576527b2ca158f47d4ef374595fde77f3a2d
Kits0n <62100445+kits0n@users.noreply.github.com>
2020-03-24 15:17:59 +0100

    Game Implementation #2

--- Client/src/sample/Controllers/GameController.java
+++ Client/src/sample/Controllers/GameController.java
@@ -3,28 +3,79 @@ package sample.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import sample.ServerCommunicator;
 
+import java.util.Arrays;
+
 
 public class GameController {
-    @FXML
-    private Label statusLabel;
-
-
-    @FXML
-    private void SendMessage(ActionEvent event) {
-
-      //  Task<Void> sendTask = new SendTask('e');
-       //statusLabel.textProperty().bind(sendTask.messageProperty());
-       // executor.submit(sendTask);
-
+    final private int size = 10;
+    private char[][] board;
+    private int pos_x;
+    private int pos_y;
+    private GameController()
+    {
+        this.board = new char[][]
+                         {{'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                         {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}};
+        System.out.print(Arrays.deepToString(board));
     }
-
+    private void Game(int y, int x)
+    {
+        board[pos_y][pos_x]=' ';
+        board[pos_y+y][pos_x+x]='p';
+        for(int i=0; i< size;i++)
+        {
+            for(int j=0; j< size;j++)
+            {
+                System.out.print(board[i][j]+" ");
+            }
+        }
+    }
     @FXML
     private void move(KeyEvent e)
     {
-        ServerCommunicator.getInstance().moveMsg(e.getCode());
+        if(e.getCode()== KeyCode.W)
+        {
+            if(pos_y!=0 && board[pos_y-1][pos_x]!='w')
+            {
+                this.Game(-1,0);
+                ServerCommunicator.getInstance().moveMsg(e.getCode());
+            }
+        }
+        else if(e.getCode()==KeyCode.A)
+        {
+            if(pos_x!=0 && board[pos_y][pos_x-1]!='w')
+            {
+                this.Game(0,-1);
+                ServerCommunicator.getInstance().moveMsg(e.getCode());
+            }
+        }
+        else if(e.getCode()==KeyCode.S)
+        {
+            if(pos_y!=size-1 && board[pos_y+1][pos_x]!='w')
+            {
+                this.Game(1,0);
+                ServerCommunicator.getInstance().moveMsg(e.getCode());
+            }
+        }
+        else if(e.getCode()==KeyCode.D)
+        {
+            if(pos_x!=size-1 && board[pos_y][pos_x+1]!='w')
+            {
+                this.Game(0,1);
+                ServerCommunicator.getInstance().moveMsg(e.getCode());
+            }
+        }
     }
-
 }
