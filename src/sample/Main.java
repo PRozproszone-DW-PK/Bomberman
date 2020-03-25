b187fb093de308a9ca1e727bef0afe8ab7bc9ddf
Kits0n <62100445+Kits0n@users.noreply.github.com>
2020-03-25 19:46:03 +0100

    Merge branch 'Client' of D:\SEMESTR IV\Przetwarzanie_Rozproszone\Bomberman\Client with conflicts.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/Menu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Game.fxml"));
         primaryStage.setScene(new Scene(root, 300, 350));
         primaryStage.setTitle("Bomberman the game");
         primaryStage.show();
